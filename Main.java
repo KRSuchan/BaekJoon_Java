ba40a2fefb7b59501e7c8da098db1e201b181e59
Suchan Lee <lsc1814@naver.com>
2023-03-17 15:38:54 +0900

    commit : solved p_10810

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("안녕하세요");
-    
+  public static void main(String[] args) throws IOException{
+    p_10810 sol = new p_10810();
+    sol.solution();
   }
 }
