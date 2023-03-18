e976ecb4dae65d6067d1a4866a6ad6052d49c577
Suchan Lee <lsc1814@naver.com>
2023-03-18 14:07:09 +0900

    commit: add p_10813

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import SOLUTIONS_code.p_10810;
 
 public class Main {
   public static void main(String[] args) throws IOException{
-    p_10810 sol = new p_10810();
+    p_10813 sol = new p_10813();
     sol.solution();
   }
 }
---
+++ SOLUTIONS_code/p_10813.java
@@ -0,0 +1,11 @@
+package SOLUTIONS_code;
+
+public class p_10813 {
+  public void solution(){
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamReader(System.out));
+    String s= br.readLine();
+    StringTokenizer st = new StringTokenizer(s, " ");
+    
+  }
+}
