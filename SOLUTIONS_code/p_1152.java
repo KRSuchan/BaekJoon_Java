245f156a62a9c138c849995452262e7369265227
Suchan Lee <lsc1814@naver.com>
2023-03-23 14:24:42 +0900

    commit: solved p_1152

--- SOLUTIONS_code/p_1152.java
+++ SOLUTIONS_code/p_1152.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class p_1152 {
-  public void solution() {
-    
+  public void solution() throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String s= br.readLine();
+    StringTokenizer st = new StringTokenizer(s, " ");
+    int words=st.countTokens();
+    bw.write(words+"");
+    bw.flush();
+    bw.close();
+    br.close();
   }
 }
