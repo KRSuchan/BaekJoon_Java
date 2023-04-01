30aa8056b0e30dcac2045c7517d73bfd996c4133
Suchan Lee <lsc1814@naver.com>
2023-04-01 16:44:36 +0900

    commit: solved p_25206

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args)throws IOException{
-    p_1316 sol = new p_1316();
+    p_25206 sol = new p_25206();
     sol.solution();
   }
 }
