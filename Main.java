e3c98b1782ae5bcb4be6e57fa2c7633a4284d466
Suchan Lee <lsc1814@naver.com>
2023-04-02 12:27:38 +0900

    commit: solved p_2738

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args)throws IOException{
-    p_25206 sol = new p_25206();
+    p_2738 sol = new p_2738();
     sol.solution();
   }
 }
---
+++ SOLUTIONS_code/p_2738.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class p_2738 {
+  public void solution() throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String s = br.readLine();
+    StringTokenizer st = new StringTokenizer(s, " ");
+    int rows =Integer.parseInt(st.nextToken());
+    int cols =Integer.parseInt(st.nextToken());
+    int[][] mat_A = new int[rows][cols];
+    int[][] mat_B = new int[rows][cols];
+    int[][] mat_Rs = new int[rows][cols];
+    for (int i = 0; i < rows; i++) {
+      s=br.readLine();
+      st= new StringTokenizer(s, " ");
+      for (int j = 0; j < cols; j++) {
+        mat_A[i][j]=Integer.parseInt(st.nextToken());
+      }
+    }
+    for (int i = 0; i < rows; i++) {
+      s=br.readLine();
+      st= new StringTokenizer(s, " ");
+      for (int j = 0; j < cols; j++) {
+        mat_B[i][j]=Integer.parseInt(st.nextToken());
+      }
+    }
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        mat_Rs[i][j]=mat_A[i][j]+mat_B[i][j];
+        bw.write(mat_Rs[i][j]+" ");
+      }
+      bw.write("\n");
+    }
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
