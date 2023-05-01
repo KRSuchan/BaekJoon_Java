e74d56a8da61c5a4cb2ed516a9025f4a3cc5f33e
Suchan Lee <lsc1814@naver.com>
2023-05-01 23:33:33 +0900

    commit : solved p_11005

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args)throws IOException{
-    p_2745 sol = new p_2745();
+    p_11005 sol = new p_11005();
     sol.solution();
   }
 }
