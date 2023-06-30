8a82c979b88ee85e9e4b346a30a526ebc48efc20
Suchan Lee <lsc1814@naver.com>
2023-06-30 16:37:19 +0900

    commit : soved p_2720

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args)throws IOException{
-    p_11005 sol = new p_11005();
+    p_2720 sol = new p_2720();
     sol.solution();
   }
 }
