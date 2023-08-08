2df21af6c5564b0dc20f6fa13561fd9cb812bfb6
Suchan Lee <lsc1814@naver.com>
2023-08-08 17:25:27 +0900

    test commit2

--- Main.java
+++ Main.java
@@ -4,19 +4,6 @@ import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int cnt = Integer.parseInt(br.readLine());
-    int[] array = new int[cnt];
-    for (int i = 0; i < cnt; i++) {
-      array[i] = Integer.parseInt(br.readLine());
-    }
-    Arrays.sort(array);
-    for (int i = 0; i < cnt; i++) {
-      bw.write(array[i]+"\n");
-    }
-    bw.flush();
-    bw.close();
-    br.close();
+
   }
 }
