8481b30bdcbec666d8542edbeb5bcc2457745a64
KRSuchan <lsc1814@naver.com>
2024-07-15 19:48:25 +0900

    commit test 3

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Main {
-    //    p_tes
+    //    p_test
     public static void main(String[] args) throws IOException {
         System.out.println();
     }
