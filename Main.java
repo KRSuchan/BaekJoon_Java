31b26786beb09b2575626aa9a922e77ed3e6ec2a
KRSuchan <lsc1814@naver.com>
2024-08-11 15:30:47 +0900

    solved p_29767 : 점수를 최대로

--- Main.java
+++ Main.java
@@ -1,34 +1,8 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    //    p_29767 : 점수를 최대로
+    //    p_
     public static void main(String[] args) throws IOException {
-        // input
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] cs = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            cs[i] = Integer.parseInt(st.nextToken());
-        }
-        // alg
-        long[] dp = new long[n];
-        dp[0] = cs[0];
-        for (int i = 1; i < n; i++) {
-            dp[i] = dp[i - 1] + cs[i];
-        }
-        Arrays.sort(dp);
-        long sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum += dp[n - 1 - i];
-        }
-        // output
-        System.out.println(sum);
+
     }
 }
---
+++ SOLUTIONS_code/P20000toP29999/p_29767.java
@@ -0,0 +1,36 @@
+package SOLUTIONS_code.P20000toP29999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class p_29767 {
+    //    p_29767 : 점수를 최대로
+    public static void solution(String[] args) throws IOException {
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] cs = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cs[i] = Integer.parseInt(st.nextToken());
+        }
+        // alg
+        long[] dp = new long[n];
+        dp[0] = cs[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + cs[i];
+        }
+        Arrays.sort(dp);
+        long sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += dp[n - 1 - i];
+        }
+        // output
+        System.out.println(sum);
+    }
+}
