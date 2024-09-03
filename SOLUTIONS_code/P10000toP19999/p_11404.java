c3abb8aacaa1907212fab46a0a29040392d81278
KRSuchan <lsc1814@naver.com>
2024-09-03 17:52:59 +0900

    solved p_11404 : 플로이드

---
+++ SOLUTIONS_code/P10000toP19999/p_11404.java
@@ -0,0 +1,46 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p_11404 {
+    //    p_11404 : 플로이드
+    static final int INF = 987654321;
+
+    public static void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] graph = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == j) graph[i][j] = 0;
+                else graph[i][j] = INF;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph[a][b] = Math.min(graph[a][b], c);
+        }
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(graph[i][j] == INF ? 0 : graph[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
