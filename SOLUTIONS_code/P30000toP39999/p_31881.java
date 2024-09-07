08dba5944791ab7db8b76632dbea75cf91b8ca43
KRSuchan <lsc1814@naver.com>
2024-09-07 21:37:15 +0900

    solved p_31881 : K512에 바이러스 퍼뜨리기

---
+++ SOLUTIONS_code/P30000toP39999/p_31881.java
@@ -0,0 +1,45 @@
+package SOLUTIONS_code.P30000toP39999;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class p_31881 {
+    //    p_31881 : K512에 바이러스 퍼뜨리기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        boolean[] infected = new boolean[N + 1];
+
+        int cnt = N;
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int q = Integer.parseInt(st.nextToken());
+            int idx;
+            switch (q) {
+                case 1:
+                    idx = Integer.parseInt(st.nextToken());
+                    if (!infected[idx]) cnt--;
+                    infected[idx] = true;
+                    break;
+                case 2:
+                    idx = Integer.parseInt(st.nextToken());
+                    if (infected[idx]) cnt++;
+                    infected[idx] = false;
+                    break;
+                case 3:
+                    sb.append(cnt).append("\n");
+                    break;
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
