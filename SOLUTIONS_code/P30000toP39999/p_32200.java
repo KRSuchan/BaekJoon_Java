cfeefd28d2fe2c3a15fe7bea5d2a703d3844660b
KRSuchan <lsc1814@naver.com>
2024-09-23 01:00:29 +0900

    p_32200 : 항해

--- SOLUTIONS_code/P30000toP39999/p_32200.java
+++ SOLUTIONS_code/P30000toP39999/p_32200.java
@@ -7,37 +7,28 @@ import java.util.StringTokenizer;
 
 public class p_32200 {
     //    p_32200 : 항해
-    public static void unsolved(String[] args) throws IOException {
+    public static void solution(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
         int Y = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        int[] sandwich = new int[N];
-        for (int i = 0; i < N; i++) {
-            sandwich[i] = Integer.parseInt(st.nextToken());
-        }
-
         int eat = 0;
-        long rest = 0;
+        int rest = 0;
+        int diff = Y - X;
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int n = sandwich[i];
+            int n = Integer.parseInt(st.nextToken());
             int e = n / X;
             int r = n % X;
             eat += e;
-            if (e != 0) {
-                if (n / e < X || n / e >= Y) {
-                    rest += n / e - Y + n % e;
-                }
-            } else {
+            if (e == 0) {
                 rest += r;
+            } else if (r > (e * diff)) {
+                rest += r - (e * diff); // 직접 식 유도함...
             }
-            System.out.println("n = " + n);
-            System.out.println("eat = " + eat);
-            System.out.println("rest = " + rest);
-            System.out.println();
         }
         System.out.println(eat);
         System.out.println(rest);
