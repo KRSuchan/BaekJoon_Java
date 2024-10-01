fa0f5c08153436bc2f59c17ef2748ea7fbb7d35b
KRSuchan <lsc1814@naver.com>
2024-10-01 18:25:15 +0900

    p_2558 : A+B - 2

---
+++ SOLUTIONS_code/P1000toP9999/p_2558.java
@@ -0,0 +1,13 @@
+package SOLUTIONS_code.P1000toP9999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p_2558 {
+    //    p_2558 : A+B - 2
+    public static void solution(String[] args) throws IOException {
+        var bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(bf.readLine()) + Integer.parseInt(bf.readLine()));
+    }
+}
