a93556357f97438210a041c053a1c496c2b18e53
KRSuchan <lsc1814@naver.com>
2024-10-02 16:19:18 +0900

    p_15894 : 수학은 체육과목 입니다

---
+++ SOLUTIONS_code/P10000toP19999/p_15894.java
@@ -0,0 +1,13 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p_15894 {
+    //    p_15894 : 수학은 체육과목 입니다
+    public static void solution(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Long.parseLong(br.readLine()) * 4);
+    }
+}
