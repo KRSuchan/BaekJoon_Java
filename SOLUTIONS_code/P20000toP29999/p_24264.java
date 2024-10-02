a79ab1a63fb1cbe18ea708c7be53359ce2adac56
KRSuchan <lsc1814@naver.com>
2024-10-02 20:02:52 +0900

    p_24264 : 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ SOLUTIONS_code/P20000toP29999/p_24264.java
@@ -0,0 +1,15 @@
+package SOLUTIONS_code.P20000toP29999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p_24264 {
+    //    p_24264 : 알고리즘 수업 - 알고리즘의 수행 시간 3
+    public static void solution(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Integer.parseInt(br.readLine());
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
