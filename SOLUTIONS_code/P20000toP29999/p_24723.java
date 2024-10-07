24b6648ea4afe171b7cfa30adfad9fe1fcbd483e
KRSuchan <lsc1814@naver.com>
2024-10-07 17:30:50 +0900

    p_24723 : 녹색거탑

---
+++ SOLUTIONS_code/P20000toP29999/p_24723.java
@@ -0,0 +1,12 @@
+package SOLUTIONS_code.P20000toP29999;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class p_24723 {
+    //    p_24723 : 녹색거탑
+    public static void solution(String[] args) throws IOException {
+        var scan = new Scanner(System.in);
+        System.out.println((int) Math.pow(2, scan.nextInt()));
+    }
+}
