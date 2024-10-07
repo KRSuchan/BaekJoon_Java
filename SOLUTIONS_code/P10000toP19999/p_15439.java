e57f19b0175f5cd66f0a276ddd22987b27ef4bf5
KRSuchan <lsc1814@naver.com>
2024-10-07 17:25:16 +0900

    p_15439 : 베라의 패션

---
+++ SOLUTIONS_code/P10000toP19999/p_15439.java
@@ -0,0 +1,13 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class p_15439 {
+    //    p_15439 : 베라의 패션
+    public static void solution(String[] args) throws IOException {
+        var scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println(x * (x - 1));
+    }
+}
