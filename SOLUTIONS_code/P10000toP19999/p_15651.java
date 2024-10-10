394f5cedbec9065003f3093eeae3cb3ba9a5b010
KRSuchan <lsc1814@naver.com>
2024-10-10 15:53:59 +0900

    p_15651 : N과 M (3)

---
+++ SOLUTIONS_code/P10000toP19999/p_15651.java
@@ -0,0 +1,37 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p_15651 {
+    //    p_15651 : N과 M (3)
+    private static StringBuilder sb;
+    private static int[] arr;
+
+    public static void solution(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        arr = new int[M];
+        dfs(N, M, 0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int n, int m, int depth) {
+        if (m == depth) {
+            for (int x : arr) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(n, m, depth + 1);
+        }
+    }
+}
