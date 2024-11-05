3820f73ccdc2f0263164d6062d35f1a074c26edf
KRSuchan <lsc1814@naver.com>
2024-11-05 17:38:31 +0900

    p_17298 : 오큰수

---
+++ SOLUTIONS_code/P10000toP19999/p_17298.java
@@ -0,0 +1,35 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class p_17298 {
+    //    p_17298 : 오큰수
+    public static void solution(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        var st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+        var sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
