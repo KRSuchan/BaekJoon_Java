c56a91c02e9ad904cf1175d0e8cde9fc57978c59
KRSuchan <lsc1814@naver.com>
2024-11-06 19:53:58 +0900

    p_14645 : 와이버스 부릉부릉

---
+++ SOLUTIONS_code/P10000toP19999/p_14645.java
@@ -0,0 +1,10 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.IOException;
+
+public class p_14645 {
+    //    p_14645 : 와이버스 부릉부릉
+    public static void solution(String[] args) throws IOException {
+        System.out.println("비와이");
+    }
+}
