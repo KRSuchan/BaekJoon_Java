c439f6654c465904fa70a8edb12016c40d4de6b5
KRSuchan <lsc1814@naver.com>
2024-11-20 11:11:10 +0900

    p_27959 : 초코바

---
+++ SOLUTIONS_code/P20000toP29999/p_27959.java
@@ -0,0 +1,17 @@
+package SOLUTIONS_code.P20000toP29999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p_27959 {
+    //    p_27959 : 초코바
+    public static void solution(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        System.out.println(N * 100 >= M ? "Yes" : "No");
+    }
+}
