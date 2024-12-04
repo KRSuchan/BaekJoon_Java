c2b0b3483b41e4762fa3c61d90b221292ad63378
KRSuchan <lsc1814@naver.com>
2024-12-04 14:58:20 +0900

    p_5337 : 웰컴

---
+++ SOLUTIONS_code/P1000toP9999/p_5337.java
@@ -0,0 +1,13 @@
+package SOLUTIONS_code.P1000toP9999;
+
+import java.io.IOException;
+
+public class p_5337 {
+    //    p_5337 : 웰컴
+    public static void solution(String[] args) throws IOException {
+        System.out.print("""
+                .  .   .
+                |  | _ | _. _ ._ _  _
+                |/\\|(/.|(_.(_)[ | )(/.""");
+    }
+}
