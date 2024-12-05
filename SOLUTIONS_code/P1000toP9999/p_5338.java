0d139400f1ac64c19329a37e0f20c048a9c5ff86
KRSuchan <lsc1814@naver.com>
2024-12-06 00:58:56 +0900

    p_5337 : 웰컴

---
+++ SOLUTIONS_code/P1000toP9999/p_5338.java
@@ -0,0 +1,16 @@
+package SOLUTIONS_code.P1000toP9999;
+
+import java.io.IOException;
+
+public class p_5338 {
+    //    p_5338 : 마이크로소프트 로고
+    public static void solution(String[] args) throws IOException {
+        System.out.println("""
+                       _.-;;-._
+                '-..-'|   ||   |
+                '-..-'|_.-;;-._|
+                '-..-'|   ||   |
+                '-..-'|_.-''-._|
+                """);
+    }
+}
