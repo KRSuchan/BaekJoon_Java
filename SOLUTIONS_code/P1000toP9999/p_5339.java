982f25ee630baef695de364e7d73577bf93e2162
KRSuchan <lsc1814@naver.com>
2024-12-07 14:45:05 +0900

    p_5339 : 콜센터

---
+++ SOLUTIONS_code/P1000toP9999/p_5339.java
@@ -0,0 +1,20 @@
+package SOLUTIONS_code.P1000toP9999;
+
+import java.io.IOException;
+
+public class p_5339 {
+    //    p_5339 : 콜센터
+    public static void solution(String[] args) throws IOException {
+        System.out.println("""
+                     /~\\
+                    ( oo|
+                    _\\=/_
+                   /  _  \\
+                  //|/.\\|\\\\
+                 ||  \\ /  ||
+                ============
+                |          |
+                |          |
+                |          |""");
+    }
+}
