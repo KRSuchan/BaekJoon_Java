9aa61337232d6224440c317b68f785775a8a5649
KRSuchan <lsc1814@naver.com>
2025-04-21 23:25:04 +0900

    p_10757 : 큰 수 A+B

---
+++ SOLUTIONS_code/P10000toP19999/p_10757.java
@@ -0,0 +1,18 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class p_10757 {
+    //    p_10757 : 큰 수 A+B
+    public static void solution(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var st = new StringTokenizer(br.readLine());
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        System.out.println(a.add(b));
+    }
+}
