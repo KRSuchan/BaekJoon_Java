0c02c833dde466e2ed7ea7ff2d057a88ab61e373
KRSuchan <lsc1814@naver.com>
2025-04-21 23:35:25 +0900

    p_10170 : NFC West vs North

---
+++ SOLUTIONS_code/P10000toP19999/p_10170.java
@@ -0,0 +1,23 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.IOException;
+
+public class p_10170 {
+    //    p_10170 : NFC West vs North
+    public static void solution(String[] args) throws IOException {
+        System.out.println("""
+                NFC West       W   L  T
+                -----------------------
+                Seattle        13  3  0
+                San Francisco  12  4  0
+                Arizona        10  6  0
+                St. Louis      7   9  0
+                
+                NFC North      W   L  T
+                -----------------------
+                Green Bay      8   7  1
+                Chicago        8   8  0
+                Detroit        7   9  0
+                Minnesota      5  10  1""");
+    }
+}
