10adf23f0215e8934e011ccb0f112b0a4f71eb3e
KRSuchan <lsc1814@naver.com>
2025-04-21 23:29:34 +0900

    p_9654 : 나부 함대 데이터

---
+++ SOLUTIONS_code/P1000toP9999/p_9654.java
@@ -0,0 +1,16 @@
+package SOLUTIONS_code.P1000toP9999;
+
+import java.io.IOException;
+
+public class p_9654 {
+    //    p_9654 : 나부 함대 데이터
+    public static void solution(String[] args) throws IOException {
+        System.out.println("""
+                SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+                N2 Bomber      Heavy Fighter  Limited    21       \s
+                J-Type 327     Light Combat   Unlimited  1        \s
+                NX Cruiser     Medium Fighter Limited    18       \s
+                N1 Starfighter Medium Fighter Unlimited  25       \s
+                Royal Cruiser  Light Combat   Limited    4        \s""");
+    }
+}
