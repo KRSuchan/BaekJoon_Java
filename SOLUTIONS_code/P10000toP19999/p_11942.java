4f9f25133e8ebdc0a4048801e0be12e2ba883a87
KRSuchan <lsc1814@naver.com>
2025-04-21 23:26:24 +0900

    p_11942 : 고려대는 사랑입니다

---
+++ SOLUTIONS_code/P10000toP19999/p_11942.java
@@ -0,0 +1,10 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.IOException;
+
+public class p_11942 {
+    //    p_11942 : 고려대는 사랑입니다
+    public static void solution(String[] args) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
