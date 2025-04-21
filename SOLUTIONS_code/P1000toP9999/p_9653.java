2a95bba49d425efcbfb802e54143f1242676995d
KRSuchan <lsc1814@naver.com>
2025-04-21 23:28:09 +0900

    p_9653 : 스타워즈 로고

---
+++ SOLUTIONS_code/P1000toP9999/p_9653.java
@@ -0,0 +1,21 @@
+package SOLUTIONS_code.P1000toP9999;
+
+import java.io.IOException;
+
+public class p_9653 {
+    //    p_9653 : 스타워즈 로고
+    public static void solution(String[] args) throws IOException {
+        System.out.println("""
+                    8888888888  888    88888
+                   88     88   88 88   88  88
+                    8888  88  88   88  88888
+                       88 88 888888888 88   88
+                88888888  88 88     88 88    888888
+                
+                88  88  88   888    88888    888888
+                88  88  88  88 88   88  88  88
+                88 8888 88 88   88  88888    8888
+                 888  888 888888888 88  88      88
+                  88  88  88     88 88   88888888""");
+    }
+}
