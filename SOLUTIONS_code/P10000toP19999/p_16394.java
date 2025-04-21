20c477e2bafb2771cb585f9e0cdbf67076709767
KRSuchan <lsc1814@naver.com>
2025-04-21 23:33:19 +0900

    p_16394 : 홍익대학교

---
+++ SOLUTIONS_code/P10000toP19999/p_16394.java
@@ -0,0 +1,13 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p_16394 {
+    //    p_16394 : 홍익대학교
+    public static void solution(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) - 1946);
+    }
+}
