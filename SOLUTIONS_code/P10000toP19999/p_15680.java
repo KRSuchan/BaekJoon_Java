978dbcadb61d37b18c0412281c3b22f30c48b6ff
KRSuchan <lsc1814@naver.com>
2025-05-27 11:44:10 +0900

    p_15680 : 연세대학교

---
+++ SOLUTIONS_code/P10000toP19999/p_15680.java
@@ -0,0 +1,16 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class p_15680 {
+    //    p_15680 : 연세대학교
+    public static void solution(String[] args) throws IOException {
+        boolean c = new Scanner(System.in).nextInt() == 0;
+        if (c) {
+            System.out.println("YONSEI");
+        } else {
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
