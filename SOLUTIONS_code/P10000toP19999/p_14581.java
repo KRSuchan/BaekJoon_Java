c7e9f6eb950d79397346390cd46e70ce92c02599
KRSuchan <lsc1814@naver.com>
2025-07-02 20:07:53 +0900

    p_14581 : 팬들에게 둘러싸인 홍준

---
+++ SOLUTIONS_code/P10000toP19999/p_14581.java
@@ -0,0 +1,15 @@
+package SOLUTIONS_code.P10000toP19999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p_14581 {
+    //    p_14581 : 팬들에게 둘러싸인 홍준
+    public static void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(":fan::fan::fan:");
+        System.out.printf(":fan::%s::fan:\n", br.readLine());
+        System.out.println(":fan::fan::fan:");
+    }
+}
